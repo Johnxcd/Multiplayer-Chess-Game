7a5d1d9087c3a1d6935c9575c78e152ffa6f0882
Riley Easton <rye.easton@gmail.com>
2024-09-22 15:30:53 -0600

    Added initial base code

---
+++ client/src/components/Page.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import { useServerSettings } from '../hooks/useServerSettings'
+
+export default function Page(props) {
+	const [serverSettings, processServerConfigSuccess] = useServerSettings(props.showMessage);
+
+	return (
+		<h1>{serverSettings.serverUrl}</h1>
+	);
+}
+
+
