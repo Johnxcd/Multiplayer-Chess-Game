9256f6904927056fab5b124c6d2295dda683ffe5
super66reaper <mattjon@colostate.edu>
2024-09-26 12:58:11 -0600

    Added Skeleton For Invitation

---
+++ server/src/main/java/com/tco/misc/Invitation.java
@@ -0,0 +1,19 @@
+package com.tco.misc;
+
+public class Invitation {
+    private String inviteMessage;
+    private String rejectedMessage;
+    // private Notification notification;
+
+    public void sendNotification(User user) {
+
+    }
+
+    public void sendInviteToPlayer(User user, Match match) {
+
+    }
+
+    public void rejectedInvitation() {
+        
+    }
+}
