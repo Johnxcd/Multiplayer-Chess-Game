e40a343e085995aef4ce2fffb27ef82112c2dc27
super66reaper <mattjon@colostate.edu>
2024-09-29 14:33:26 -0600

    Added Profile Skeleton and Fixed some of Invitation Skeleton

---
+++ server/src/test/java/com/tco/misc/TestProfile.java
@@ -0,0 +1,16 @@
+package com.tco.misc;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestProfile {
+    private Profile profile;
+
+    @BeforeEach
+    public void setUp() {
+        profile = new Profile();
+    }
+}
