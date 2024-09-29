bdacea1413d19b9a99bfc6d68ecda9f891558b58
Johnxcd <johnh9@maserati.cs.colostate.edu>
2024-09-29 13:43:02 -0600

    junit test class

---
+++ server/src/test/java/com/tco/misc/TestUser.java
@@ -0,0 +1,18 @@
+package com.tco.misc;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestUser {
+
+    private User user;
+
+    @BeforeEach
+    public void setUp() {
+        user = new User();
+    }
+
+}
