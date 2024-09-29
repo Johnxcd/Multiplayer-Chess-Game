b5e1c00dd2df05cd09c87d7e3c119fd6402f2912
Dureke <dureke.sanchez2@gmail.com>
2024-09-29 09:52:04 -0500

    Added skeleton Platform class

---
+++ server/src/main/java/com/tco/platform/Platform.java
@@ -0,0 +1,25 @@
+package com.tco.platform;
+
+public class Platform {
+    // private Profile profile;
+
+    // private void sendInvitation(Profile profile) {
+
+    // }
+
+    private void displayNotifications() {
+
+    }
+
+    // private void displayHistory(Profile profile) {
+
+    // }
+
+    private void login(String username, String password) {
+
+    }
+
+    private void logout() {
+        
+    }
+}
