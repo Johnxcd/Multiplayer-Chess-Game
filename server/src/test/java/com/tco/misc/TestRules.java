111b1142ce8380a61920ab1d8b975945d4a74cf5
Johnxcd <johnh9@maserati.cs.colostate.edu>
2024-09-29 13:51:36 -0600

    Rules class skeleton

---
+++ server/src/test/java/com/tco/misc/TestRules.java
@@ -0,0 +1,29 @@
+package com.tco.misc;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestRules {
+/* 
+    private Rules rules;
+    private Game game;
+    private Move move;
+    private Player player;
+    private List<Player> players;
+    private List<GameState> gameStates;
+
+    @BeforeEach
+    public void setUp() {
+        rules = new Rules();
+        game = new Game();
+        move = new Move();
+        player = new Player();
+        players = new ArrayList<>();
+        gameStates = new ArrayList<>();
+    }
+    */
+
+}
