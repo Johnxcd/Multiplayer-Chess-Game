d28edbdb5b480820db97cc383e453643a938d0d1
turnred <112022897+turnred@users.noreply.github.com>
2024-10-03 09:49:15 -0600

    Create Match.java

---
+++ server/src/main/java/com/tco/misc/Match.java
@@ -0,0 +1,26 @@
+package com.tco.misc;
+
+import java.util.List;
+
+public class Match {
+    private List<User> users;
+    private History history;
+    private Rules rules;
+    int[][] board; //TODO: change this to Piece once class is made
+
+    public void createBoard() {
+        //TODO: fill in
+    }
+
+    public void getMove(List<User> users) {
+        //TODO: fill in
+    }
+
+    public void endMatch() {
+        //TODO: fill in
+    }
+
+    public void saveToHistory(List<User> users, History history) {
+        //TODO: fill in
+    }
+}
