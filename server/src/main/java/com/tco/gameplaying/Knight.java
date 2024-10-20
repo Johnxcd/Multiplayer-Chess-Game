19e6b4fabe22e7b568cef5851fbdf3ea59ff51b2
Johnxcd <johnh9@moth.cs.colostate.edu>
2024-10-20 17:28:36 -0600

    Knight movement and test class

--- server/src/main/java/com/tco/gameplaying/Knight.java
+++ server/src/main/java/com/tco/gameplaying/Knight.java
@@ -2,15 +2,36 @@ package com.tco.gameplaying;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.logging.Logger;
 import com.tco.gamemanagement.Game;
 
-public class Knight extends Piece{
-    public Knight(Color color, int[] position){
+public class Knight extends Piece {
+    private static final Logger logger = Logger.getLogger(Knight.class.getName());
+
+    public Knight(Color color, int[] position) {
         super(PieceType.KNIGHT, color, position);
     }
 
     @Override
     public List<int[]> getPossibleMoves(Game game) {
-        return new ArrayList<>(); // placeholder for knight's possible moves
+        List<int[]> moves = new ArrayList<>();
+        int[][] directions = {
+            {2, 1}, {2, -1}, {-2, 1}, {-2, -1},
+            {1, 2}, {1, -2}, {-1, 2}, {-1, -2}
+        };  // possible L-shaped moves
+
+        for (int[] direction : directions) {
+            int[] pos = {getPos()[0] + direction[0], getPos()[1] + direction[1]};
+            if (game.isPositionOnBoard(pos)) {
+                Piece pieceAtPos = game.getPieceAt(pos);
+                logger.info("Checking position: [" + pos[0] + ", " + pos[1] + "]");
+                logger.info("Piece at position: " + pieceAtPos);
+                if (pieceAtPos == null || pieceAtPos.getColor() != this.getColor()) {
+                    moves.add(pos);  // add move if empty or occupied by opponents piece
+                }
+            }
+        }
+        logger.info("Total possible moves: " + moves.size());
+        return moves;
     }
 }
