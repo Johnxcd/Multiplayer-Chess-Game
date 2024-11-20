88a0ab008e92b96e27b7db1eec4df29ed3383b46
Ethan Turner <ept@termite.cs.colostate.edu>
2024-11-20 12:51:48 -0700

    Schema validation and fixes

--- server/src/test/java/com/tco/misc/TestJSONValidator.java
+++ server/src/test/java/com/tco/misc/TestJSONValidator.java
@@ -1,6 +1,9 @@
 package com.tco.misc;
 
 import com.tco.requests.ConfigRequest;
+import com.tco.requests.GameRequest;
+import com.tco.requests.MoveRequest;
+import com.tco.requests.UserRequest;
 
 import java.lang.reflect.Type;
 
@@ -42,6 +45,42 @@ public class TestJSONValidator {
     @DisplayName("base: There should be no schema for the JSONValidator class")
     public void testMissingSchema() {
         test("", JSONValidator.class, false);
+    }
+
+    @Test
+    @DisplayName("ept: Game Request should pass schema validation")
+    public void testGameRequestPass() {
+        test("{\"requestType\":\"game\",\"id\":\"0\",\"action\":\"create\",\"users\":[\"\"]}", GameRequest.class, true);
+    }
+
+    @Test
+    @DisplayName("ept: Game Request should fail schema validation")
+    public void testGameRequestFail() {
+        test("", GameRequest.class, false);
+    }
+
+    @Test
+    @DisplayName("ept: Move Request should pass schema validation")
+    public void testMoveRequestPass() {
+        test("{\"requestType\":\"move\",\"uuid\":\"testid\",\"from\":[0,0],\"to\":[0,0]}", MoveRequest.class, true);
+    }
+
+    @Test
+    @DisplayName("ept: Move Request should fail schema validation")
+    public void testMoveRequestFail() {
+        test("", MoveRequest.class, false);
+    }
+
+    @Test
+    @DisplayName("ept: User Request should pass schema validation")
+    public void testUserRequestPass() {
+        test("{\"requestType\":\"user\",\"action\":\"anystring\"}", UserRequest.class, true);
+    }
+
+    @Test
+    @DisplayName("ept: User Request should fail schema validation")
+    public void testUserRequestFail() {
+        test("", UserRequest.class, false);
     }
 
     @Test
